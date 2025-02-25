f5a241cdf5d70531e54ee14db89f894eedaaa954
Naysa Chopra <naysachopra@gmail.com>
2025-02-24 19:35:49 -0800

    finally set something up

---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Navbar.css';  // Import the specific CSS file
+
+const Navbar = () => {
+  return (
+    <div className="navbar-container">
+        <div className="home-button">
+            <a href="/" className="navbar-content">HOME</a>
+        </div>
+        <div className="other-buttons">
+            <a href="" className="navbar-content">FINANCIAL</a>
+            <a href="" className="navbar-content">ANALYSIS</a>
+            <a href="" className="navbar-content">STORIES</a>
+        </div>
+    </div>
+  );
+}
+
+export default Navbar;
