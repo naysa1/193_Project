b323870eb5da2d049604ce2673ee1f3a67eb40d3
Naysa Chopra <naysachopra@gmail.com>
2025-02-25 18:11:02 -0800

    fixing about and navbar

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+// import React from 'react';
+// import './Navbar.css';  // Import the specific CSS file
+
+// const Navbar = () => {
+//   return (
+//     <div className="navbar-container">
+//         <div className="home-button">
+//             <a href="/" className="navbar-content">HOME</a>
+//         </div>
+//         <div className="other-buttons">
+//             <a href="" className="navbar-content">FINANCIAL</a>
+//             <a href="" className="navbar-content">ANALYSIS</a>
+//             <a href="" className="navbar-content">STORIES</a>
+//         </div>
+//         <div className="empty-space"></div>
+//     </div>
+//   );
+// }
+
+// export default Navbar;
+
+import React, { useState, useEffect } from 'react';
 import './Navbar.css';  // Import the specific CSS file
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 80) {
+        setIsScrolled(true); // Change navbar style when scrolled
+      } else {
+        setIsScrolled(false); // Revert to default
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="navbar-container">
+    <div className={`navbar-container ${isScrolled ? "scrolled" : ""}`}>
         <div className="home-button">
             <a href="/" className="navbar-content">HOME</a>
         </div>
@@ -18,3 +54,4 @@ const Navbar = () => {
 }
 
 export default Navbar;
+
